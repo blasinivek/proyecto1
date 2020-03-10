bb8fd1641fe05f8595b514012df274a42a05052c
kevin lopez <kevinlopezreyes1@gmail.com>
2020-03-10 02:37:12 -0300

    firstcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+// For MDB Angular Free
+import { CarouselModule, WavesModule,CollapseModule  } from 'angular-bootstrap-md'
 
 import { AppComponent } from './app.component';
+import { ServiciosComponent } from './servicios/servicios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiciosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarouselModule, WavesModule,CollapseModule ,
+    MDBBootstrapModule.forRoot()
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
